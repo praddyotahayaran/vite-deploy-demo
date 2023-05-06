73e6d442ede69f614357f6c3c71c99418ac46f51
Praddyota <praddyotahayaran100@gmail.com>
2023-05-06 08:02:54 +0530

    some changes

--- src/App.jsx
+++ src/App.jsx
@@ -8,8 +8,6 @@ const App = () => {
     <div className='app'>
       <NavBar />
       <Home />
-      <Home />
-      <Home />
     </div>
   )
 }
--- src/Components/Home/Home.jsx
+++ src/Components/Home/Home.jsx
@@ -1,47 +1,25 @@
 import React from 'react'
 import './Home.css'
-import thumb from '../../assets/thumb.jpg'
+
+import { Parallax, ParallaxLayer } from '@react-spring/parallax'
 
 const Home = () => {
     return (
-        <div className='home'>
-            <h1 className='home__heading'>Product Overview</h1>
+        <div className="home">
+            <Parallax pages={4}>
 
-            <div className="home__card">
-                <div className="home__card-img">
-                    <div className="home__card-creative">
+                <ParallaxLayer>
+                    WEB DEVELOPMENT WEB
+                </ParallaxLayer>
 
-                        <img src={thumb} alt="" />
-                        <h3>Full Stack Developer</h3>
-
-                        <button className="home__card-btnProfile">
-                            View Profile
-                        </button>
-                        <div className="stats">
-                            <span>86% Rating</span>
-                            <span>92% Activity</span>
-                        </div>
-                    </div>
-                </div>
-
-                <div className="home__card-text">
-                    <h2>People</h2>
-                    <p>
-                        A next generation HRIS that forms the core of
-                        your people stack and programmatically drives
-                        your people services, from time off to access
-                        management, payroll to benefits. Minimise time
-                        spent on admin using automation and
-                        employee self-service.
-                    </p>
-
-                    <button className="home__card-btn">
-                        Find out more
-                    </button>
-                </div>
-            </div>
+                <ParallaxLayer>
+                    WEB DEVELOPMENT WEB
+                </ParallaxLayer>
+            </Parallax>
         </div>
     )
 }
 
 export default Home
+
+
